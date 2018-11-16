f24bd9207efe093f6d7700ef33d3de2946bda4c7
slarribeau <slarribeau@gmail.com>
2018-11-16 15:14:58 -0800

    promises baby

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,38 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
+    async onSearchSubmit(term) {
         console.log("onSearchSubmit")
-        axios.get('https://api.unsplash.com/search/photos', {
+        console.log(term)
+        console.log("onSearchSubmit")
+
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params:{query: term},
             headers: {
                 Authorization: 'Client-ID 0f4944d511713570f8578ef6b127964ab1018df030e19ec64f2ecca83f107abc'
 
             }
         });
+        console.log(response.data.results);
     }
+
+    /*class App extends React.Component {
+        onSearchSubmit(term) {
+            console.log("onSearchSubmit")
+            console.log(term)
+            console.log("onSearchSubmit")
+    
+            axios.get('https://api.unsplash.com/search/photos', {
+                params:{query: term},
+                headers: {
+                    Authorization: 'Client-ID 0f4944d511713570f8578ef6b127964ab1018df030e19ec64f2ecca83f107abc'
+    
+                }
+            })
+            .then(response => {
+                console.log(response.data.results);
+            });
+        }*/
 
     render() {
         return (
