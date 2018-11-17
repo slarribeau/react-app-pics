c26873d2f23467f3c252a567f572206fdbe56801
slarribeau <slarribeau@gmail.com>
2018-11-17 08:21:49 -0800

    finish grid display

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {spans:0};
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imageRef.current.clientHeight);
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+        this.setState({spans});
+    }
+
     render() {
-        console.log(this.props.image)
+        const {description, urls} = this.props.image;
         return (
-            <div>
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
                 <img 
-                    alt={this.props.image.description}
-                    src={this.props.image.urls.regular}
+                    ref = {this.imageRef}
+                    alt={description}
+                    src={urls.regular}
                 />
             </div>
         );
