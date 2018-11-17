1c8bb866bca7731bd760252e88f4886c156c7252
slarribeau <slarribeau@gmail.com>
2018-11-17 07:32:59 -0800

    add imageCard.js

---
+++ src/components/ImageCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+    render() {
+        console.log(this.props.image)
+        return (
+            <div>
+                <img 
+                    alt={this.props.image.description}
+                    src={this.props.image.urls.regular}
+                />
+            </div>
+        );
+    }
+}
+export default ImageCard;
